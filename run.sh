50a0334e0050fa336532913e615fa01fa4c5d53b
Dave Burke <dburke84@gmail.com>
2015-08-19 21:52:16 -0500

    Run the container as a daemon

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker run --name torrent --rm=true -it -v /mnt/media/torrent:/var/lib/transmission-daemon -p 9091:9091 -p 51413:51413 torrent
+docker run --name torrent -d -v /mnt/media/torrent:/var/lib/transmission-daemon -p 9091:9091 -p 51413:51413 torrent
 
