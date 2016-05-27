d3336b12f5ab81bc42434e6a75a1a39f0109de47
Dave Burke <dburke84@gmail.com>
2016-05-26 19:31:48 -0500

    Fix `docker ps` syntax in run.sh

--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 name="torrent"
-cid="$(docker ps -q ${name})"
+cid="$(docker ps -q --filter "name=${name}")"
 if [ -n "${cid}" ]; then
 	echo "Stopping container..."
 	docker stop "${name}" > /dev/null
 fi
-cid="$(docker ps -a -q ${name})"
+cid="$(docker ps -a -q --filter "name=${name}")"
 if [ -n "${cid}" ]; then
 	echo "Removing container..."
 	docker rm "${name}" > /dev/null
